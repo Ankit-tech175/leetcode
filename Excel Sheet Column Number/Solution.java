82169f55dec3b01652b1aa450a97c98ff313da10
Ankit <ankitchauhan19092005@gmail.com>
2025-09-09 22:10:58 +0530

    code

---
+++ Excel Sheet Column Number/Solution.java
@@ -0,0 +1,15 @@
+
+
+             // Excel Sheet Column Number - 171 leetcode problem
+
+public class Solution {
+    public int titleToNumber(String columnTitle) {
+        int res=0;
+        for(int i=0;i<columnTitle.length();i++){
+            int d= columnTitle.charAt(i)-'A'+1;
+            res=res*26 + d;
+        }
+        return res;
+    }
+}
+
