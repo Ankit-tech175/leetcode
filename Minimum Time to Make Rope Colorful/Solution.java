dadf237529f7d681c8a2e50549183620bf95254a
Ankit <ankitchauhan19092005@gmail.com>
2025-11-03 12:57:55 +0530

    code

---
+++ Minimum Time to Make Rope Colorful/Solution.java
@@ -0,0 +1,17 @@
+
+
+
+             // 1578. Minimum Time to Make Rope Colorful
+
+public class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int t = 0;
+        for (int i = 1; i < colors.length(); i++) {
+            if (colors.charAt(i) == colors.charAt(i-1)) {
+                t += Math.min(neededTime[i], neededTime[i-1]);
+                neededTime[i] = Math.max(neededTime[i], neededTime[i-1]);
+            }
+        }
+        return t;
+    }
+}
