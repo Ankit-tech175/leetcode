9a5c89d704d19e29bb357e6294e48587a67544aa
Ankit <ankitchauhan19092005@gmail.com>
2025-11-08 23:04:52 +0530

    code

---
+++ Binary Tree Inorder Traversal/Solution.java
@@ -0,0 +1,36 @@
+
+
+
+         // Binary Tree inorder Traversal -94 problem
+
+import java.util.*;
+public class Solution {
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> l= new ArrayList<>();
+        inorder(root,l);
+        return l;
+    }
+    private  void inorder(TreeNode root,List<Integer> l){
+       if(root== null){
+            return;
+       }
+       inorder(root.left,l);
+       l.add(root.val);
+       inorder(root.right,l);
+    }
+}
+    
